c824be04b3406b02c2f3d10a4bf517f43107a22a
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2025-02-01 18:01:10 -0800

    Refactoring data model for test-train split, keep together for each random sample/bootstrap.  Next fix mnist_common, then mnist_kmeans, then mnist_simgraphs, then mnist_tuning.  Then finish single/pairwise/multi digit examples (Show the "best" results, show digitwise spectra (diff between k and k+1 eigenvalues?) vs test/train accuracy.) for each.

--- spectral_clustering/mnist_data.py
+++ spectral_clustering/mnist_data.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 import logging
@@ -13,29 +14,21 @@ class MNISTData(object):
     Load data, combine test/train sets. 
     """
 
-    def __init__(self):
+    def __init__(self, pca_dim=0):
+        self._pca_dim = pca_dim
         (x_train, y_train), (x_test, y_test) = load_cached(self._read_mnist,
                                                            "MNIST_raw_data.pkl")
+        self.train = {d: np.array([img.reshape(-1) for img in x_train[y_train == d]]) for d in range(10)}
+        self.test = {d: np.array([img.reshape(-1) for img in x_test[y_test == d]]) for d in range(10)}
+        #self.print_digit_stats()
 
-        x = np.vstack((x_train, x_test))
-        y = np.concatenate((y_train, y_test))
-        self._digits = {d: x[y == d] for d in range(10)}
-        self._digits_flat = {d: x[y == d].reshape((len(x[y == d]), -1)) for d in range(10)}
-        logging.info("Loaded MNIST data, %i samples." % (len(y), ))
-
-    def get_digit_sample_inds(self, d, n_sample=0):
+    def print_digit_stats(self):
         """
-        Get indices of n_sample samples of digit d.
-        :param d: digit
-        :param n_sample: number of samples to get. If 0, get all.
-        :return: indices of samples
+        print number of training and test images per digit.
         """
-        n = len(self._digits[d])
-        if n < n_sample:
-            raise ValueError("Too many samples requested.")
-        elif n_sample == 0:
-            return np.arange(n)
-        return np.random.choice(n, n_sample, replace=False)
+        logging.info("MNIST data digit stats:")
+        for d in range(10):
+            logging.info("\t%i: train %i, test %i" % (d, self.train[d].shape[0], self.test[d].shape[0]))
 
     def _read_mnist(self):
         # Import here because it's slow and unnecessary if
@@ -43,61 +36,104 @@ class MNISTData(object):
         from keras.api.datasets import mnist
         return mnist.load_data()
 
-    def get_digit(self, d, n_sample=0):
-        return self._digits_flat[d]
-
-    def get_images(self, d):
-        return self._digits[d]
-
-
-class MNISTDataPCA(MNISTData):
-    def __init__(self, dim=30, **kwargs):
+    def get_sample(self, n_train, n_test=0, digits=None):
         """
-        :param dim: number of PCA components to keep, or 0 for no PCA.
-        :param kwargs: passed to sklearn.decomposition.PCA
+        Get random samples.
         """
-        self._d = dim
-        super(MNISTDataPCA, self).__init__()
-        if self._d > 0:
-            self._reduce_dim()
+        return MNISTSample(self, digits,
+                           pca_dim=self._pca_dim,
+                           n_train=n_train,
+                           n_test=n_test)
 
-    def _reduce_dim(self):
-        logging.info("Computing PCA with %s components." % (self._d, ))
-        self._pca = PCA(n_components=self._d, whiten=True)  # few percent better without whitening
-        self._pca.fit(np.vstack([self.get_digit(d) for d in range(10)]))
 
-        logging.info("\tPCA complete.")
-        self._digits_flat = {d: self._pca.transform(self.get_digit(d)) for d in range(10)}
+class MNISTSample(object):
+    """
+    Class to hold a test/train split of MNIST data and manage PCA.
+    Also, keep track of sample indices so the image can be retrieved from the full data.
+    (Don't store anything large, since we want to pickle this object.)
+    """
 
-        all_flats = np.vstack([self._digits_flat[d] for d in range(10)])
-        cov = np.cov(all_flats.T)
+    def __init__(self, data, digits, pca_dim, n_train, n_test):
+        """
+        :param data: MNISTData object
+        :param digits: iterable of digits to include
+        :param pca_dim: PCA dimension (computed on training set of each sample)
+        :param n_train: number of training samples per digit
+        :param n_test: number of test samples per digit, or -1 for all  
+        """
+        self.dim, self.pca_dim = data.train[0].shape[1], pca_dim
+        self.digits = digits if digits is not None else tuple([i for i in range(10)])
+
+        self.train_inds = {d: np.random.choice(data.train[d].shape[0], n_train, replace=False)
+                           for d in self.digits}
+        if n_test == -1:
+            self.test_inds = {d: np.arange(data.test[d].shape[0],dtype=np.int32) for d in self.digits}
+        else:
+            self.test_inds = {d: np.random.choice(data.test[d].shape[0], n_test, replace=False)
+                            for d in self.digits}
+
+        if self.pca_dim > 0:
+            logging.info("Computing PCA with %s components." % (self.pca_dim, ))
+            self._pca = PCA(n_components=self.pca_dim, whiten=False)  # few percent better without whitening
+            train_x = np.vstack([data.train[d][self.train_inds[d]] for d in self.digits])
+            self._pca.fit(train_x)
+            var_sum = np.sum(self._pca.explained_variance_ratio_[:self.pca_dim])
+            sd_range = np.sqrt(self._pca.explained_variance_ratio_[0]), np.sqrt(self._pca.explained_variance_ratio_[-1])
+            logging.info("\tPCA complete, sd[0]=%.3f, sd[%i]=%.5f, %.3f %% of variance." %
+                         (sd_range[0], self.pca_dim, sd_range[1], var_sum*100))
+
+            self.train = {d: self._pca.transform(data.train[d][self.train_inds[d]]) for d in self.digits}
+            self.test = {d: self._pca.transform(data.test[d][self.test_inds[d]]) if self.test_inds[d].size > 0
+                             else np.zeros((0, self.pca_dim))
+                         for d in self.digits}
+        else:
+            self.train = {d: data.train[d][self.train_inds[d]] for d in self.digits}
+            self.test = {d:  data.test[d][self.test_inds[d]] if self.test_inds[d].size > 0 else np.zeros((0, self.dim))
+                        for d in self.digits}               
+
+    def get_data(self, which='train'):
+        """
+        Get the test/training data in single arrays.
+        """
+
+        def _get_pair(source):
+            x = np.vstack([source[d] for d in self.digits])
+            y = np.hstack([d*np.ones(source[d].shape[0], dtype=np.int32) for d in self.digits])
+            return x, y
+
+        if which == 'train':
+            return _get_pair(self.train)
+        elif which == 'test':
+            return _get_pair(self.test)
+        else:
+            raise ValueError("Unknown data type: %s (should be 'test' or 'train')" % which)
 
 
 def test_data(plot=False):
-    MNISTData()
-    pca_data = MNISTDataPCA(dim=2)
+    fig,ax=plt.subplots()
+    pca_data = MNISTData(pca_dim=2).get_sample(2000, 0)
     if not plot:
         return
-
-    import matplotlib.pyplot as plt
     for d in range(10):
-        plt.scatter(pca_data.get_digit(d)[:, 0], pca_data.get_digit(d)[:, 1], label=str(d), s=1)
+        x = pca_data.train[d]
+        ax.scatter(x[:, 0], x[:, 1], label=str(d), s=1)
     plt.legend()
     plt.title("MNIST data, 2 principal components")
-    plt.show()
 
 
-def test_data_img():
+def test_data_img(plot=True):
     """
     Make a collage of data.
     """
     n_rows = 4
     n_cols = 4
-    data = MNISTDataPCA(dim=30)
+    data = MNISTData(pca_dim=0).get_sample(100, 0)
+
 
     imgs = {}
+    n_imgs_per_digit = n_rows*n_cols
     for d in range(10):
-        images = data.get_images(d)
+        images = data.train[d][:n_imgs_per_digit].reshape(-1, 28, 28)
         imgs[d] = np.zeros((28*n_rows, 28*n_cols), dtype=np.uint8)
         for i in range(n_rows):
             for j in range(n_cols):
@@ -111,21 +147,31 @@ def test_data_img():
         if x >= output.shape[1]:
             x = 0
             y += imgs[d].shape[0]
+    if plot:
+        
+        title = "MNIST data"
+        fig, ax = plt.subplots()
+        ax.imshow(1-output/255., cmap='gray')
+        ax.set_title(title)
+        # axes off
+        ax.axis('off')
+        plt.tight_layout()
 
-    title = "MNIST data"
-    import matplotlib.pyplot as plt
-    fig, ax = plt.subplots()
-    ax.imshow(1-output/255., cmap='gray')
-    ax.set_title(title)
-    # axes off
-    ax.axis('off')
-    plt.tight_layout()
-    plt.show()
-
+def test_all(plot=False):
+    # test pca
+    MNISTData(30).get_sample(1000, 100)
+    MNISTData(50).get_sample(1000, 100)
+    MNISTData(100).get_sample(1000, 100)
+    # show sample digits
+    test_data_img(plot=plot)
+    # show data on 2 pca axes
+    test_data(plot=plot)
+    if plot:
+        plt.show()
 
 if __name__ == "__main__":
 
     logging.basicConfig(level=logging.INFO)
-    test_data_img()
-    test_data(True)
+    logging.info("Testing MNIST data.")
+    test_all(plot=True)
     logging.info("Done.")
