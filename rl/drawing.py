97eeccfd2104c0f014f4fb50aed314d3fadba571
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2025-03-26 19:27:13 -0700

    fogot the star

--- rl/drawing.py
+++ rl/drawing.py
@@ -1,49 +1,140 @@
 import numpy as np
-from game_base import Mark
+from game_base import Mark, get_cell_center
 from util import get_annulus_polyline
 import cv2
-from colors import MPL_BLUE_RGB, MPL_GREEN_RGB, MPL_ORANGE_RGB
+from colors import COLOR_X, COLOR_O, COLOR_BG
+
+_SHIFT_BITS = 5
+_SHIFT = 1 << _SHIFT_BITS
+
+
+def draw_win_line(img, dims, line, color):
+    """
+    Draw a line through three cells.
+    :param img: np.array, image to draw on (single 3x3 game)
+    :param dims: image dimensions dict, output of Game.get_image_dims(), used to create img
+    :param line: element of list from Game._get_win_lines(), a dict with
+        'orient': one of 'h','v','d'
+        'c1': (i,j) cell coordinates (0, 1 2)
+        'c2': (i,j) cell coordinates
+    :param color: tuple, (r,g,b) color of the line
+    """
+    space_size = dims['space_size']
+
+    cell1= line['c1']
+    cell2= line['c2']
+    cell_span1 = dims['cells'][cell1[0]][cell1[1]]
+    cell_span2 = dims['cells'][cell2[0]][cell2[1]]
+
+    if line['orient'] == 'h':
+        x0 = int((cell_span1['x'][0]) * _SHIFT)
+        x1 = int((cell_span2['x'][1]) * _SHIFT)
+        y = int(((cell_span1['y'][0] + cell_span2['y'][1]-1)/2) * _SHIFT)
+        cv2.line(img, (x0, y), (x1, y), color, dims['line_t'], lineType=cv2.LINE_AA, shift=_SHIFT_BITS)
+    elif line['orient'] == 'v':
+        x = int(((cell_span1['x'][0] + cell_span1['x'][1]-1)/2) * _SHIFT)
+        y0 = int((cell_span1['y'][0]) * _SHIFT)
+        y1 = int((cell_span2['y'][1]) * _SHIFT)
+        cv2.line(img, (x, y0), (x, y1), color, dims['line_t'], lineType=cv2.LINE_AA, shift=_SHIFT_BITS)
+    elif line['orient'] == 'd':
+        # Need to take different corners of the cells to get the diagonal
+        if cell1[1]< cell2[1]:
+            x0 = int((cell_span1['x'][0]) * _SHIFT)
+            x1 = int((cell_span2['x'][1]) * _SHIFT)
+            y0 = int((cell_span1['y'][0]) * _SHIFT)
+            y1 = int((cell_span2['y'][1]) * _SHIFT)
+        else:
+            x0 = int((cell_span1['x'][1]) * _SHIFT)
+            x1 = int((cell_span2['x'][0]) * _SHIFT)
+            y0 = int((cell_span1['y'][0]) * _SHIFT)
+            y1 = int((cell_span2['y'][1]) * _SHIFT)
+
+
+        cv2.line(img, (x0, y0), (x1, y1), color, dims['line_t'], lineType=cv2.LINE_AA, shift=_SHIFT_BITS)
+    return img
+
 
 class MarkerArtist(object):
     """
-    Draw "X", "O", circle, square, or "D" (for draw) on an image.
-    Scale appropriately.
-    """
+    Draw X O Markers on the image:
+        * Normal sized markers:  X and O with scaled thickness  (space size >= 10)
+        * Small markers: X and O with single-pixel lines (5 <= space_size < 10)
+        * Tiny markers: fill the cell completely with the color   (space_size < 5)
 
-    def __init__(self, color_x=MPL_BLUE_RGB, color_o=MPL_ORANGE_RGB, color_d=MPL_GREEN_RGB):
+    Marker size is determined by the space size it's being drawn in.
+    This value should be in dims['space_size'].
+
+    Normal sized markers will be padded by empty space, determined by dims['marker_padding_frac'].
+
+    """
+    _CUTOFFS = [13, 5]
+
+    def __init__(self, color_x=COLOR_X, color_o=COLOR_O):
         self._font = cv2.FONT_HERSHEY_COMPLEX
-        self._small_cutoff_cell_size = 15
-        self.color_o = color_o
-        self.color_x = color_x
-        self.color_d = color_d
+        self.colors = {Mark.O: color_o,
+                       Mark.X: color_x}
         self._SHIFT_BITS = 5
         self._SHIFT = 1 << self._SHIFT_BITS
-        self._point_cache = {}  #
+        self._point_cache = {}  # The "O" polyline to fill, for each value of dims['space_size']
 
-    def add_marker(self, img, center, space_size, marker, pad_frac=0.1):
+    def add_marker(self, img, dims, loc, marker):
         """
         :param img: np.array, image to draw on
-        :param center: tuple, center of the marker
-        :param space_size: int, number of pixels height/width of the grid cell (not counting lines)
-        :param marker: str, "X", "O", "D"
-        :param pad_frac: float, fraction of space_size to pad around the marker
+        :param dims: image dimensions dict, output of Game.get_image_dims()
+        :param loc: tuple, (i,j) location in the 3x3 grid, (0,1,2) for each
+        :param marker: str, "X", "O"
         """
-        x, y = center
+        cell_span = dims['cells'][loc[0]][loc[1]]
+        space_size = dims['space_size']
+        padding_px = int(space_size * dims['marker_padding_frac'])
+
+        if dims['space_size'] >= self._CUTOFFS[0]:  # draw Normal
+            if marker == Mark.X:
+
+                x0, x1 = cell_span['x'][0]+padding_px//2, cell_span['x'][1]-padding_px//2-1
+                y0, y1 = cell_span['y'][0]+padding_px//2, cell_span['y'][1]-padding_px//2-1
+                cv2.line(img, (x0, y0), (x1, y1), self.colors[marker], dims['line_t'], lineType=cv2.LINE_AA)
+                cv2.line(img, (x1, y0), (x0, y1), self.colors[marker], dims['line_t'], lineType=cv2.LINE_AA)
+
+            else:
+                center = get_cell_center(dims, loc)
+                x, y = center
+                rad = (space_size/2)-padding_px/2 +1
+                rad_inner = rad - (dims['line_t']*1.3)
+                cv2.circle(img, (int(x*self._SHIFT), int(y*self._SHIFT)), int(rad*self._SHIFT),
+                           self.colors[marker],thickness=-1, lineType=cv2.LINE_AA, shift=self._SHIFT_BITS)
+                cv2.circle(img, (int(x*self._SHIFT), int(y*self._SHIFT)), int(rad_inner*self._SHIFT),
+                           COLOR_BG, thickness=-1,lineType=cv2.LINE_AA, shift=self._SHIFT_BITS)
+
+        elif dims['space_size'] >= self._CUTOFFS[1]:  # draw Small
+            # Draw single-pixel wide lines with 1- or 2-pixel  margin.
+
+            m_pix = 1 if dims['space_size'] < np.mean(self._CUTOFFS) else 2
+
+            center = get_cell_center(dims, loc)
+            x, y = center
+            if marker == Mark.X:
+                x0, x1 = cell_span['x'][0]+m_pix, cell_span['x'][1]-1-m_pix
+                y0, y1 = cell_span['y'][0]+m_pix, cell_span['y'][1]-1-m_pix
+
+                cv2.line(img, (x0, y0), (x1, y1), self.colors[marker], 1, lineType=cv2.LINE_AA)
+                cv2.line(img, (x1, y0), (x0, y1), self.colors[marker], 1, lineType=cv2.LINE_AA)
+
+            else:
+                center = get_cell_center(dims, loc)
+                x, y = center
+                rad = (space_size/2)-m_pix
+                cv2.circle(img, (int(x*self._SHIFT), int(y*self._SHIFT)), int(rad*self._SHIFT),
+                           self.colors[marker], lineType=cv2.LINE_AA, shift=self._SHIFT_BITS)
+
+        else:
+            # fill the cell with the color
+            x_span, y_span = cell_span['x'], cell_span['y']
+            img[y_span[0]:y_span[1], x_span[0]:x_span[1]] = self.colors[marker]
+        return img
         pad_size = int(space_size * pad_frac)
         marker_size = space_size - 2 * pad_size
         center_int = (int(x), int(y))
-
-        if marker == "X":
-            color = self.color_x
-            dot_shape = 'x' if space_size >= self._small_cutoff_cell_size else 'square'
-        elif marker == "O":
-            color = self.color_o
-            dot_shape = 'o' if space_size >= self._small_cutoff_cell_size else 'circle'
-        elif marker == "D":
-            dot_shape = 'd'
-            color = self.color_d
-            font_scale = space_size / 50
-            font_thickness = max(1, int(space_size / 20))
 
         # half the width of the different markers:
         circle_rad = np.max([2, marker_size//1.6]).astype(int)
@@ -59,25 +150,17 @@ class MarkerArtist(object):
             cv2.circle(img, center_int, circle_rad, color, -1)
 
         elif dot_shape == 'x':
-            x, y = int(x), int(y)
-            cv2.line(img, (x - x_rad, y - x_rad), (x + x_rad, y + x_rad), color, line_width)
-            cv2.line(img, (x + x_rad, y - x_rad), (x - x_rad, y + x_rad), color, line_width)
+            pass
 
         elif dot_shape == 'o':
             r_inner = circle_rad - line_width
             points = (self._get_o_points(circle_rad, r_inner) + np.array(center).T) * self._SHIFT
             cv2.fillPoly(img, [points.astype(np.int32)], color, lineType=cv2.LINE_AA, shift=self._SHIFT_BITS)
 
-        elif dot_shape == 'd':
-            (width, height), baseline = cv2.getTextSize("D", self._font, font_scale, font_thickness)
-            t_x = center[0] - width//2
-            t_y = center[1] + height//2
-            pos = int(t_x), int(t_y)
-            cv2.putText(img, "D", pos, self._font, font_scale, color, font_thickness, lineType=cv2.LINE_AA)
-
     def _get_o_points(self, size, line_width):
+
         if size not in self._point_cache:
-            self._point_cache[size] = get_annulus_polyline(size, line_width)
+            self._point_cache[size] = get_annulus_polyline(size, line_width, n_points=int(size*2))
         return self._point_cache[size]
 
 
@@ -92,36 +175,36 @@ def test_terminals():
 
     show_strs(["XOO",
                "OXX",
-               "XOO"], bbox = True)
+               "XOO"], bbox=True)
     show_strs(["XOO",
                "OXX",
-               "XOO"], bbox = False)
+               "XOO"], bbox=False)
     show_strs(["XOO",
                "XXX",
-               "XOO"], bbox = True)
+               "XOO"], bbox=True)
     show_strs(["XOO",
                "XXX",
-               "XOO"], bbox = False)
+               "XOO"], bbox=False)
     show_strs(["XOO",
                " OX",
-               "XOO"], bbox = True)
+               "XOO"], bbox=True)
     show_strs(["XOO",
                " OX",
-               "XOO"], bbox = False)
+               "XOO"], bbox=False)
     show_strs(["XXO",
                "XOX",
-               "OOO"], bbox = True)
+               "OOO"], bbox=True)
     show_strs(["XXO",
                "XOX",
-               "OOO"], bbox = False)
-    
+               "OOO"], bbox=False)
+
     show_strs(["XXO",
                "X X",
-               "O O"], bbox = True)
+               "O O"], bbox=True)
     show_strs(["XXO",
                "X X",
-               "O O"], bbox = False)
-    
+               "O O"], bbox=False)
+
     cv2.destroyAllWindows()
 
 
@@ -141,10 +224,10 @@ def test_img():
     widths = []
     for i, size in enumerate(sizes):
         x_offset = np.ceil(np.sum(widths) + i * padding).astype(int)
-        game_img = game.get_img(space_size=size, marker_padding_frac=0.15)
+        game_img = game.get_img(space_size=size)
         w = game_img.shape[1]
         img[:w, x_offset:x_offset + w,] = game_img
-        print(game_img.dtype)
+
         widths.append(w)
 
     cv2.imwrite("game.png", img)
@@ -153,10 +236,7 @@ def test_img():
     cv2.destroyAllWindows()
 
 
-
-
-
 if __name__ == "__main__":
-    #test_img()
+    # test_img()
     test_terminals()
     # get_draw()
