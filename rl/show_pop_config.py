f73d22d86468579f1de3de0a2d1785eb79a935bf
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2025-06-15 19:02:23 -0700

    Added show-genome to show_pop_config.py, should change name to show_info.py

--- rl/show_pop_config.py
+++ rl/show_pop_config.py
@@ -4,7 +4,8 @@ import os
 import sys
 from evolve_feedforward import StdOutReporterWGenomSizesPlus
 import logging
-
+import numpy as np
+import pickle
 
 def get_config(obj):
     config = {}
@@ -18,16 +19,42 @@ def get_config(obj):
     return config
 
 
-def _show_config(filename):
+def _show_pop_config(filename):
     pop = neat.Checkpointer.restore_checkpoint(filename)
     config = pop.config
-    print("Configuration for NEAT:")
     print("========================================")
-    print(f"Population file: {filename}")
-    print("========================================")
-    print("Configuration parameters:")
+    print(f"Configuration for NEAT population file: {filename}")
     print("========================================")
     pprint.pprint(get_config(config))
+    print("========================================")
+    
+def _show_genome_config(filename):
+    """
+    Read the genome, print the number of input, output, hidden nodes,
+    print the number of connections, then fraction that are enabled.
+    """
+    with open(filename,'rb') as infile:
+        genome = pickle.load(infile)
+    
+    print("========================================")
+    print(f"Configuration for NEAT Genome: {filename}")
+    print("========================================")
+    #import ipdb; ipdb.set_trace()
+    connection_origins = [connect_key[0] for connect_key in genome.connections]
+    connection_dests = [node_key for node_key in genome.nodes]
+    n_inputs = -np.min(connection_origins)
+    # find the highest consecutive node id
+    n_outputs = np.where(np.diff(connection_dests)>1)[0][0] + 1
+    n_hidden = len(connection_dests )-n_outputs
+    hidden_ids = [node_id for node_id in connection_dests if node_id >= n_outputs]
+    hidden_conns = [conn for conn in genome.connections if conn[0] in hidden_ids or conn[1] in hidden_ids]
+    print(f"Number of input nodes:  {n_inputs}")
+    print(f"Number of output nodes: {n_outputs}")
+    print(f"Number of hidden nodes: {n_hidden}")
+    print(f"Number of connections: {len(genome.connections)}")
+    print(f"Number of hidden_connections: {len(hidden_conns)}")
+    enabled_connections = sum(1 for conn in genome.connections if genome.connections[conn].enabled)
+    print(f"Enabled connections: {enabled_connections} ({enabled_connections / len(genome.connections):.2%})")
     print("========================================")
     
 
@@ -36,12 +63,15 @@ def show_config():
     try:
         filename = sys.argv[1]
     except Exception:
-        print("Usage: python show_config.py <population_file>")
+        print("Usage: python show_config.py <population_file/winner_file>")
         sys.exit(1)
     if not os.path.exists(filename):
         print(f"File {filename} does not exist.")
         sys.exit(1)
-    _show_config(filename)
+    if filename.endswith('.pkl'):
+        _show_genome_config(filename)
+    else:
+        _show_pop_config(filename)
 
 
 if __name__== "__main__":
